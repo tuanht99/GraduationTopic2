a1b1c8aae67597f4b68bfab6211dea74f05f465a
Duy Khanh <dzerok2020@gmail.com>
2022-09-28 18:01:52 +0700

    setup project: firebase, prettier, eslint, base react native

--- Freentship/App.js
+++ Freentship/App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, SafeAreaView, StatusBar, Platform } from 'react-native'
+import { HomeScreen } from './src/screens/HomeScreen'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={styles.container}>
+      <HomeScreen />
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
+  }
+})
