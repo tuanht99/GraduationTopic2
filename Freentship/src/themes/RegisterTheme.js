6ef5e624b4c8bed6521f19abf715e958b9aa3093
thanhlk0803 <20211TT1281@mail.tdc.edu.vn>
2022-10-10 13:07:53 +0700

    add giao diện

---
+++ Freentship/src/themes/RegisterTheme.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Dimensions } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    paddingTop: 70,
+    flex: 1,
+  },
+
+
+});
+
