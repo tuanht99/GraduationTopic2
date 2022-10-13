0daa17e6b65579a2c9013eb90eca99ee6fbe21c7
TranQuocHuy <20211tt1283@mail.tdc.edu.vn>
2022-10-14 01:35:41 +0700

    Update DetailFood && Store

--- Freentship/App.js
+++ Freentship/App.js
@@ -1,7 +1,8 @@
 
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import Router from './src/routes';
 import {food} from './src/screens/Store/FoodFirebase'
+import firestore from '@react-native-firebase/firestore';
 export default function App() {
   return (
     <View style={styles.container}>
